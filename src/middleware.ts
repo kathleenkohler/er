1e3b305bc2d63ae6b7be7cd59d87e83f2c5b3f5b
Kathleen Kohler <katiikohler@gmail.com>
2025-04-23 23:40:29 -0400

    feat(middleware): verify JWT in protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,51 @@
+import { NextRequest, NextResponse } from "next/server";
+import { jwtVerify } from "jose";
 import createMiddleware from "next-intl/middleware";
+import { cookies } from "next/headers";
 
-export default createMiddleware({
-  // TODO: When the translations are ready, add the 'es' locale
-  // A list of all locales that are supported
+const intlMiddleware = createMiddleware({
   locales: ["en", "es"],
-
-  // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
   defaultLocale: "en",
 });
+
+const protectedRoutes = ['/user', '/user/shared', '/es/user', '/es/user/shared']
+const publicRoutes = ['/login', '/register', '/es/login', '/es/register']
+
+async function verificarToken(token: string) {
+  try {
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
+    const { payload } = await jwtVerify(token, secret);
+    return payload;
+  } catch (err) {
+    return null;
+  }
+}
+
+export async function middleware(req: NextRequest) {
+  const intlResponse = intlMiddleware(req);
+  const path = req.nextUrl.pathname
+  
+  // Verificación de token JWT
+  const cookieStore = cookies();
+  const token = cookieStore.get('token')?.value;
+  const valid = token ? await verificarToken(token) : null;
+  
+  //Ruta privada sin token
+  if (!valid && protectedRoutes.includes(path)) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  // Si la ruta es pública pero loggeado
+  if (publicRoutes.includes(path) && valid) {
+    return NextResponse.redirect(new URL('user', req.url));
+  }
+
+  if (intlResponse) {
+    return intlResponse
+  }
+
+  return NextResponse.next()
+ }
 
 export const config = {
   // Skip all paths that should not be internationalized. This example skips the
