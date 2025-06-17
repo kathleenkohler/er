dffb806609e397a9dfb95e6f49390f7f184d07da
Kathleen Kohler <katiikohler@gmail.com>
2025-06-16 20:10:12 -0400

    move MONGO_URI to env file

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,17 +1,14 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = "mongodb://localhost:27017/ERdocdb";
-
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export async function connectToDatabase() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI, {
+    cached.promise = mongoose.connect(process.env.MONGO_URI!, {
       dbName: "ERdocdb",
     } as any).then(() => console.log("Connected to MongoDB!"));
-    //.then((mongoose) => {return mongoose});
   }
 
   cached.conn = await cached.promise;
