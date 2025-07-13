9b9da557fa73219cf135782ce7c0cb4c3ec88d37
Kathleen Kohler <katiikohler@gmail.com>
2025-07-13 14:19:35 -0400

    test: ensure error is thrown when aggregation includes a weak entity

---
+++ test/ERDoc/linter/aggregation_includes_weak_entity.test.ts
@@ -0,0 +1,48 @@
+import { ER } from "../../../src/ERDoc/types/parser/ER";
+import { checkAggregationIncludesWeakEntity } from "../../../src/ERDoc/linter/aggregation/checkAggregationIncludesWeakEntity";
+import { parse } from "../../../src/ERDoc/parser";
+
+describe("Linter detects when an aggregation includes a relationship with a weak entity", () => {
+  it("returns an error when an aggregation includes a weak entity", () => {
+    const errors = checkAggregationIncludesWeakEntity(AggregationWithWeakEntityER);
+    expect(errors.length).toBe(1);
+    expect(errors[0].type).toBe("AGGREGATION_INCLUDES_WEAK_ENTITY");
+    expect(errors[0].aggregationName).toBe("inscripcion_detallada");
+    expect(errors[0].relationshipName).toBe("inscripcion");
+    expect(errors[0].weakEntityName).toBe("detalle");
+  });
+  it("returns no errors when all entities are strong", () => {
+    const errors = checkAggregationIncludesWeakEntity(AggregationNoWeakEntitiesER);
+    expect(errors.length).toBe(0);
+  });
+});
+
+const AggregationWithWeakEntityER: ER = parse(`
+entity curso {
+  id key
+  name
+}
+
+entity detalle depends on inscripcion {
+  horario pkey
+}
+
+relation inscripcion (curso, detalle)
+
+aggregation inscripcion_detallada(inscripcion)
+`);
+
+const AggregationNoWeakEntitiesER: ER = parse(`
+entity curso {
+  id key
+}
+
+entity estudiante {
+  id key
+}
+
+relation inscripcion (curso, estudiante)
+
+aggregation inscripcion_detallada(inscripcion)
+`);
+
